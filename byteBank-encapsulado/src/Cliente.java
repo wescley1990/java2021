bb286765ba1336116135162b5a95e5a90eabddbc
Wescley <wescley.batista1@aluno.unip.br>
2021-03-09 17:09:13 -0300

    get e set no titular

--- byteBank-encapsulado/src/Cliente.java
+++ byteBank-encapsulado/src/Cliente.java
@@ -1,8 +1,35 @@
 
 public class Cliente {
-	String Nome;
-	String CPF;
-	String Profissão;
-	String Endereço;
+	private String Nome;
+	private String CPF;
+	private String Profissão;
+	private String Endereço;
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getProfissão() {
+		return Profissão;
+	}
+	public void setProfissão(String profissão) {
+		Profissão = profissão;
+	}
+	public String getEndereço() {
+		return Endereço;
+	}
+	public void setEndereço(String endereço) {
+		Endereço = endereço;
+	}
+	
+	
+	
 	
 }
--- byteBank-encapsulado/src/Conta.java
+++ byteBank-encapsulado/src/Conta.java
@@ -45,4 +45,10 @@ public class Conta {
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	public Cliente getTitular() {
+		return this.titular;
+	}
 }
