dd537a36f319dbd7521fb81d01791b4ba168c4bb
Wescley <wescley.batista1@aluno.unip.br>
2021-03-09 17:08:45 -0300

    Get e set - no titular

--- byteBank-encapsulado/src/TestaGetESet.java
+++ byteBank-encapsulado/src/TestaGetESet.java
@@ -6,5 +6,22 @@ public class TestaGetESet {
 		//conta.numero = 7777;
 		conta.setNumero(7777);
 		System.out.println(conta.getNumero());
+		
+		Cliente Wescley = new Cliente();
+		Wescley.setNome("Wescley Alves Batista");
+		
+		conta.setTitular(Wescley);
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissão("Programador");
+		// agora em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissão("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(Wescley);
+		System.out.println(conta.getTitular());
+		
 	}
+	
 }
