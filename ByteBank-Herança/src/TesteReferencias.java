26758980ecb0655ff95493edad2b3d64985ed1c5
Wescley <wescley.batista1@aluno.unip.br>
2021-04-19 15:43:53 -0300

    corrigindo as referencias

---
+++ ByteBank-Herança/src/TesteReferencias.java
@@ -0,0 +1,12 @@
+
+public class TesteReferencias {
+	
+	public static void main(String[] args) {
+		
+		Gerente g1 = new Gerente();
+		g1.setNome("Wescley");
+		String. = g1.getNome();
+		
+		System.out.println(nome);
+	}
+}
